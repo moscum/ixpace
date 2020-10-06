653a740b875731c909eaedfc9739b6b857220430
penguin <vanechka0101@outlook.com>
2020-10-06 19:04:57 +0500

    added about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './style.scss';
 import Header from "./components/header/header";
 import Honestly from "./components/honestly/honestly";
+import About from "./components/about/about";
 
 function App() {
   return (
     <div className="wrapper">
         <Header/>
         <Honestly/>
+        <About/>
     </div>
   );
 }
---
+++ src/components/about/about.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import styles from './about.module.scss'
+import logo from "../header/logo.png";
+import laptop from "./laptop.png"
+
+class About extends Component {
+    render() {
+        return (
+            <section className={styles.bg + " section"}>
+                <div className="container">
+                    <div className={styles.title}>
+                        <img src={logo} alt="logo"/>
+                        <h1>- ПЛАТФОРМА ДЛЯ ЛЮДЕЙ</h1>
+                    </div>
+                    <h2 className={styles.subtitle}>
+                        Она ломает привычные вам шаблоны о получении прибыли online<br/>
+                        и включает в себя:
+                    </h2>
+                    <div className={styles.col}>
+                        <div className={styles.row}>
+                            <img className={styles.img} src={laptop} alt="laptop"/>
+                            <div className={styles.col__text}>
+                                <p className={styles.text}>Систему обучения</p>
+                                <p className={styles.text}>Сайты и посадочные страницы для привлечения клиентов </p>
+                                <p className={styles.text}>Встроенную СRM-систему
+                                    и систему аналитики</p>
+                                <p className={styles.text}>Сопровождение куратора
+                                    до получения прибыли</p>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <p className={styles.text__c}>Социальную экосистему, которая
+                                объединяет людей с общими интересами
+                                и взглядами на жизнь</p>
+                            <p className={styles.text__c}>Готовую масштабируемую бизнес-модель
+                                для запуска своего online-бизнеса
+                                в любой точке мира</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default About;
