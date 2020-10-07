2dba782a03cdef10e44ee88291f88203d3f11609
penguin <vanechka0101@outlook.com>
2020-10-08 02:45:18 +0500

    added steps section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Honestly from "./components/honestly/honestly";
 import About from "./components/about/about";
 import Availability from "./components/availability/availability";
 import Platform from "./components/platform/platform";
+import Steps from "./components/steps/steps";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <About/>
         <Availability/>
         <Platform/>
+        <Steps/>
     </div>
   );
 }
